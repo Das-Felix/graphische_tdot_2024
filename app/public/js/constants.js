e225250f5ce214f17a64f791e1c7d9f08e35c2e0
DasFelix_ <Raffelsberger.felix@gmx.at>
2024-10-08 12:50:02 +0200

    feat: DEV and TDOT Mode, TDOT Hotkeys

--- app/public/js/constants.js
+++ app/public/js/constants.js
@@ -1,2 +1,3 @@
 export const MAX_CAMS  = 3;
 export const SCORE_INTERVAL = 10;
+export const MODE = "DEV";
--- app/public/js/controller/inputHandler.js
+++ app/public/js/controller/inputHandler.js
@@ -1,13 +1,39 @@
-import {MAX_CAMS} from "/js/constants.js";
+import {MAX_CAMS, MODE} from "/js/constants.js";
 
 export function listenForInputs() {
 
+    if(MODE === "DEV") {
+        document.addEventListener("keydown", (event) => {
+            for(let i = 1; i <= MAX_CAMS; i++) {
+                if(event.key === i.toString()) {
+                    document.dispatchEvent(new CustomEvent("selectCam", {detail: i}));
+                }
+            }
+
+            if(event.key === "Enter") {
+                document.dispatchEvent(new CustomEvent("confirmPressed"));
+            }
+        });
+
+        return;
+    }
+
     document.addEventListener("keydown", (event) => {
+
+        //Cam 1
+        const camKeys = {
+            1: ["Backspace", "Alt", "-", "*"],
+            2: ["+", "PageUp", "ArrowRight"],
+            3: ["Enter", "PageDown", "Delete"]
+        }
+
         for(let i = 1; i <= MAX_CAMS; i++) {
-            if(event.key === i.toString()) {
+            if(camKeys[i].includes(event.key)) {
                 document.dispatchEvent(new CustomEvent("selectCam", {detail: i}));
             }
         }
+
+        const confirmKeys = ["Insert", "End", "ArrowLeft", "Home"]
 
         if(event.key === "Enter") {
             document.dispatchEvent(new CustomEvent("confirmPressed"));
